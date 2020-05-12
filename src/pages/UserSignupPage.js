658b7886228cba7ba3e8202884e43758309886e9
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-05-12 19:41:18 -0300

    Add basic signup page

--- src/pages/UserSignupPage.js
+++ src/pages/UserSignupPage.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const UserSignupPage = () => {
+const UserSignupPage = (props) => {
+
+  const {actions} = props
+
+  const [displayName, setDisplayName] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [repeatPassword, setRepeatPassword] = useState('')
+
+
+  const onClickSignup = () => {
+    const user = {
+      username,
+      displayName,
+      password
+    }
+    actions.postSignup(user)
+  }
+
   return (
     <div>
       <h1>Sign up</h1>
+
+      <div>
+        <input 
+          value={displayName} 
+          onChange={e => setDisplayName(e.target.value)} 
+          type="text" placeholder="Your display name"/>
+        <input 
+          value={username} 
+          onChange={e => setUsername(e.target.value)} 
+          type="text" placeholder="Your username" />
+        <input 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+          type="password" name="" id="" placeholder="Your password" />
+        <input 
+          value={repeatPassword} 
+          onChange={(e) => setRepeatPassword(e.target.value)} 
+          type="password" name="" id="" placeholder="Repeat your password" />
+
+        <button onClick={onClickSignup} type="submit">Sign Up</button>
+      </div>
     </div>
   )
+}
+
+UserSignupPage.defaultProps = {
+  actions: {
+    postSignup: () => new Promise((resolve, reject) => {
+      resolve({})
+    })
+  }
 }
 
 export default UserSignupPage
--- src/pages/UserSignupPage.spec.js
+++ src/pages/UserSignupPage.spec.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {render} from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import UserSignupPage from './UserSignupPage'
 
@@ -11,5 +11,174 @@ describe('UserSignupPage', () => {
 
       expect(header).toHaveTextContent('Sign up')
     })
+
+    it('has input for display name', () => {
+      const {queryByPlaceholderText} = render(<UserSignupPage />)
+      const displayNameInput = queryByPlaceholderText('Your display name')
+
+      expect(displayNameInput).toBeInTheDocument();
+    })
+
+    it('has input for username', () => {
+      const {queryByPlaceholderText} = render(<UserSignupPage />)
+      const usernameInput = queryByPlaceholderText('Your username')
+
+      expect(usernameInput).toBeInTheDocument();
+    })
+
+    it('has input for password', () => {
+      const {queryByPlaceholderText} = render(<UserSignupPage />)
+      const passwordInput = queryByPlaceholderText('Your password')
+
+      expect(passwordInput).toBeInTheDocument();
+    })
+
+    it('has password type for password input', () => {
+      const {queryByPlaceholderText} = render(<UserSignupPage />)
+      const passwordInput = queryByPlaceholderText('Your password')
+
+      expect(passwordInput.type).toBe('password')
+    })
+
+    it('has input for password repeat', () => {
+      const {queryByPlaceholderText} = render(<UserSignupPage />)
+      const passwordInput = queryByPlaceholderText('Repeat your password')
+
+      expect(passwordInput).toBeInTheDocument();
+    })
+
+    it('has input for password repeat', () => {
+      const {queryByPlaceholderText} = render(<UserSignupPage />)
+      const passwordInput = queryByPlaceholderText('Repeat your password')
+
+      expect(passwordInput.type).toBe('password');
+    })
+
+    it('has submit button', () => {
+      const {container } = render(<UserSignupPage />)
+      const button = container.querySelector('button')
+
+      expect(button).toBeInTheDocument();
+    })
+
+    it('has submit button of type submit', () => {
+      const {container} = render(<UserSignupPage />)
+      const button = container.querySelector('button')
+
+      expect(button.type).toBe('submit')
+    })
+
+    it('has submit button with text Sign Up', () => {
+      const {container} = render(<UserSignupPage />)
+      const button = container.querySelector('button')
+
+      expect(button).toHaveTextContent('Sign Up')
+    })
+
+  })
+
+  describe('Interactions', () => {
+    const changeEvent = content => {
+      return {
+        target: {
+          value: content
+        }
+      }
+    }
+
+    let button, displayNameInput, usernameInput, passwordInput, repeatPasswordInput
+
+    const setupForSubmit = (props) => {
+      const rendered = render(<UserSignupPage {...props} />)
+
+      const {container, queryByPlaceholderText} = rendered
+
+      displayNameInput = queryByPlaceholderText('Your display name')
+      usernameInput = queryByPlaceholderText('Your username')
+      passwordInput = queryByPlaceholderText('Your password')
+      repeatPasswordInput = queryByPlaceholderText('Repeat your password')
+
+      fireEvent.change(displayNameInput, changeEvent('my-display-name'))
+      fireEvent.change(usernameInput, changeEvent('my-user-name'))
+      fireEvent.change(passwordInput, changeEvent('P4ssword'))
+      fireEvent.change(repeatPasswordInput, changeEvent('P4ssword'))
+
+      button = container.querySelector('button')
+
+      return rendered
+    }
+
+    it('sets the displayName value into state', () => {
+      const { queryByPlaceholderText } = render(<UserSignupPage />)
+      const displayNameInput = queryByPlaceholderText('Your display name')
+
+      fireEvent.change(displayNameInput, changeEvent('my-display-name'))
+
+      expect(displayNameInput).toHaveValue('my-display-name')
+    })
+
+    it('sets the username value into state', () => {
+      const {queryByPlaceholderText} = render(<UserSignupPage />)
+      const usernameInput = queryByPlaceholderText('Your username')
+
+      fireEvent.change(usernameInput, changeEvent('my-username'))
+      expect(usernameInput).toHaveValue('my-username')
+    })
+
+    it('sets the password value into the state', () => {
+      const { queryByPlaceholderText} = render(<UserSignupPage />)
+      const passwordInput = queryByPlaceholderText('Your password')
+
+      fireEvent.change(passwordInput, changeEvent('P4ssword'))
+      expect(passwordInput).toHaveValue('P4ssword')
+    })
+
+    it('sets the repeatPassword value into the state', () => {
+      const { queryByPlaceholderText} = render(<UserSignupPage />)
+      const repeatPasswordInput = queryByPlaceholderText('Repeat your password')
+
+      fireEvent.change(repeatPasswordInput, changeEvent('P4ssword'))
+      expect(repeatPasswordInput).toHaveValue('P4ssword')
+    })
+
+    it('calls postSignUp when the fields are valid and the actions are provided in props', () => {
+      const actions = {
+        postSignup: jest.fn().mockResolvedValueOnce({})
+      }
+
+      setupForSubmit({actions})
+      fireEvent.click(button)
+
+      expect(actions.postSignup).toHaveBeenCalledTimes(1)
+
+    })
+
+    it('does not throw an exception when clicking the button when actions not provided in props calls postSignUp when the fields are valid and the actions are provided in props', () => {
+
+      setupForSubmit()
+
+      expect(() => fireEvent.click(button)).not.toThrow()
+
+
+    })
+
+    it('calls post with user body when the fields are valid', () => {
+      const actions = {
+        postSignup: jest.fn().mockResolvedValueOnce({})
+      }
+
+      setupForSubmit({actions})
+      fireEvent.click(button)
+
+      const expectedUserObject = {
+        username: 'my-user-name',
+        displayName: 'my-display-name',
+        password: 'P4ssword',
+      }
+
+      expect(actions.postSignup).toHaveBeenCalledWith(expectedUserObject)
+
+    })
+
   })
 })
